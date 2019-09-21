180c8ab10f5a89ad19f4bd1e8c56f7aae4f2c124
yong.wang <yong.wang@ucarinc.com>
2019-09-21 22:15:26 +0800

    通过props组件，进行组件复用

--- src/ch02/PostList.js
+++ src/ch02/PostList.js
@@ -1,42 +1,38 @@
 import React, { Component } from 'react'
 
+import PostItem from './PostItem'
+
+const data = [
+  {
+    title: '大家一起来讨论React吧',
+    author: '张三',
+    date: '2019-09-09 12:23:45'
+  },
+  {
+    title: '前端框架,你最爱哪一种',
+    author: '李四',
+    date: '2019-09-10 12:23:45'
+  },
+  {
+    title: 'Web App的时代已经结束',
+    author: '王五',
+    date: '2019-09-19 12:23:45'
+  }
+]
+
 class PostList extends Component {
   render () {
     return (
       <div>
         帖子列表:
         <ul>
-          <li>
-            <div>大家一起来讨论React吧</div>
-            <div>
-              创始人:<span>张三</span>
-            </div>
-            <div>
-              创建时间:<span>2019-09-09 12:23:45</span>
-            </div>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <div>前端框架,你最爱哪一种</div>
-            <div>
-              创始人:<span>李四</span>
-            </div>
-            <div>
-              创建时间:<span>2019-09-10 12:23:45</span>
-            </div>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            Web App的时代已经结束
-            <div>
-              创始人:<span>王五</span>
-            </div>
-            <div>
-              创建时间:<span>2019-09-19 12:23:45</span>
-            </div>
-          </li>
+          {data.map((item) => (
+            <PostItem
+              author={item.author}
+              title={item.title}
+              date={item.date}
+            />
+          ))}
         </ul>
       </div>
     )
