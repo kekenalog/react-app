ac5e7ba610462971b5aa37946bc61562a099116c
yong.wang <yong.wang@ucarinc.com>
2019-09-22 19:53:28 +0800

    内嵌样式--方式style声明

--- src/ch02/Welcome.js
+++ src/ch02/Welcome.js
@@ -1,8 +1,18 @@
 import React from 'react'
-import './style.css'
 
 function Welcome (props) {
-  return <h1 className="foo">Hello, {props.name}</h1>
+  return (
+    <h1
+      style={{
+        width: '100%',
+        height: '50px',
+        backgroundColor: 'green',
+        fontSize: '20px'
+      }}
+    >
+      Hello, {props.name}
+    </h1>
+  )
 }
 
 Welcome.defaultProps = {
