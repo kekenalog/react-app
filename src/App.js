235f6c78d910c4a27c56ce3e856c89b9aa5dd0b1
yong.wang <yong.wang@ucarinc.com>
2019-09-26 18:48:14 +0800

    错误边界

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-import ListComponent from './ch03/ListComponent'
-import StringComponent from './ch03/StringComponent'
+import ErrorBoundary from './ch03/ErrorBoundary'
 
-function App () {
-  return [
-    <ul>
-      <ListComponent />
-    </ul>,
-    <StringComponent />
-  ]
+const Profile = ({ user }) => <div>name: {user}</div>
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { user: { name: 'react' } }
+  } // 将user置为null，模拟异常
+  onClick = () => {
+    this.setState({ user: null })
+  }
+
+  render() {
+    return (
+      <div>
+        <ErrorBoundary>
+          <Profile user={this.state.user} />
+        </ErrorBoundary>
+        <button onClick={this.onClick}>更新</button>
+      </div>
+    )
+  }
 }
-
 export default App
