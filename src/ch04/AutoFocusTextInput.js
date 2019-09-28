242184a22aa6fd0a7923084c1603ffcd70475926
yong.wang <yong.wang@ucarinc.com>
2019-09-28 21:07:02 +0800

    在组件上使用ref

--- src/ch04/AutoFocusTextInput.js
+++ src/ch04/AutoFocusTextInput.js
@@ -1,20 +1,55 @@
 import React, { Component } from 'react'
 
 class AutoFocusTextInput extends React.Component {
-  componentDidMount () {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
     // 通过ref让input自动获取焦点
     this.textInput.focus()
   }
 
-  render () {
+  // 让input失去焦点
+  blur() {
+    this.textInput.blur()
+  }
+
+  render() {
     return (
       <div>
         <input
           type="text"
+          onBlur={this.blur}
           ref={(input) => {
             this.textInput = input
           }}
         />
+      </div>
+    )
+  }
+}
+
+class Container extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  handleClick = () => {
+    // 通过ref调用AutoFocusTextInput组件的方法
+    this.inputInstance.blur()
+  }
+
+  render() {
+    return (
+      <div>
+        {' '}
+        <AutoFocusTextInput
+          ref={(input) => {
+            this.inputInstance = input
+          }}
+        />{' '}
+        <button onClick={this.handleClick}>失去焦点</button>{' '}
       </div>
     )
   }
